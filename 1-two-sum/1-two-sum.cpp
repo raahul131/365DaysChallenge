40ecc7ed402244cf497694d1f4a790048e68623f
Rahul R Yadav <107882121+raahul131@users.noreply.github.com>
2022-10-04 13:38:32 +0530

    Time: 14 ms(86.89%), space: 11 MB (32.90%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++)
         {
-            // here we find int the hash table, if target is found after target-nums[i] in the  hash table then we store it in the ans, and after that store its index value also  
+            // here we find int the hash table, if target iss found after target-nums[i] in the  hash table then we store it in the ans, and after that store its index value also  
             if(m.find(target-nums[i])!=m.end())
             {
                 v.push_back(m[target-nums[i]]);
